5c08ea7eba86b9dd8ad709a738b459df4ed34472
jesus love <snowley777@gmail.com>
2023-06-08 19:47:56 -0500

    Created an Apollo Provider to make every request work with the Apollo Server

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,55 @@
 import React from 'react';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SearchBooks from './pages/SearchBooks';
-import SavedBooks from './pages/SavedBooks';
-import Navbar from './components/Navbar';
+
+import SearchBook from './pages/SearchBooks';
+import SavedBook from './pages/SavedBooks';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <Routes>
-          <Route 
-            path='/' 
-            element={<SearchBooks />} 
-          />
-          <Route 
-            path='/saved' 
-            element={<SavedBooks />} 
-          />
-          <Route 
-            path='*'
-            element={<h1 className='display-2'>Wrong page!</h1>}
-          />
-        </Routes>
-      </>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+            <Routes>
+              <Route 
+                path="/" 
+                element={<SearchBook />} 
+              />
+              <Route 
+                path="/saved" 
+                element={<SavedBook />} 
+              />
+            </Routes>
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
